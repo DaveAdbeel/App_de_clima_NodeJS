5e6d7124e3e501797b4179b41c60ede363b3da21
David Astudillo <davidadbeelgonzalez@gmail.com>
2022-07-14 15:08:09 -0400

    adding methods from searches class

--- app.js
+++ app.js
@@ -6,40 +6,55 @@ import Searches from "./models/searches.js";
 
 let opt = null;
 const searches = new Searches();
-
+searches.readDB();
 (async () => {
     do {
         opt = await menu();
-        
         switch (opt) {
             case 1:
-                //Mostrar mensaje
+                //Show message for write the place
                 const city = await readInput("Ciudad:");
+                //Search the place
                 const cities = await searches.searchCities(city);
+                //List the place
                 const id = await placeList(cities);
-                if (id === 0) break;
-                const selPlace = cities.find(l => l.id === id)
-                const weather = await searches.cityTemp(selPlace.lat, selPlace.lng);
-                //Buscar lugares
-                
-                //Seleccionar el lugar
+                if (id === 0) continue;
+                //Search the place by id
+                const selPlace = cities.find((l) => l.id === id);
+                //Load in db the place
+                searches.addHistory(selPlace.place_name);
+                //Weather info with the lat and lon of the place
+                const weather = await searches.cityTemp(
+                    selPlace.lat,
+                    selPlace.lng
+                );
 
-                //Mostrar informacion del lugar
-                
-                console.log('\n==============================='.green);
+                //Show info of the place
+                console.clear();
+                console.log("\n===============================".green);
                 console.log(`   Informacion de la ciudad`.green);
-                console.log('===============================\n'.green);
-                console.log(`${'Ciudad: '.blue} ${selPlace.place_name}`);
-                console.log(`${'Lat:'.yellow} ${selPlace.lat}`);
-                console.log(`${'Lng:'.yellow} ${selPlace.lng}`);
-                console.log(`${'Temperatura:'.dim} ${weather.temp}`);
-                console.log(`${'Minima:'.cyan} ${weather.temp_min}`);
-                console.log(`${'Maxima:'.red} ${weather.temp_max}`);
-                console.log(`${'Descripcion:'.green} ${weather.description}`);
+                console.log("===============================\n".green);
+                console.log(`${"Ciudad: ".blue} ${selPlace.place_name}`);
+                console.log(`${"Lat:".yellow} ${selPlace.lat}`);
+                console.log(`${"Lng:".yellow} ${selPlace.lng}`);
+                console.log(`${"Temperatura:".dim} ${weather.temp}`);
+                console.log(`${"Minima:".cyan} ${weather.temp_min}`);
+                console.log(`${"Maxima:".red} ${weather.temp_max}`);
+                console.log(`${"Descripcion:".green} ${weather.description}`);
                 break;
+
+            case 2:
+                console.clear();
+                console.log("==============".green);
+                console.log("  Historial".green)
+                console.log("==============\n".green);
                 
-                case 2:
-                    break;
+                    searches.history.forEach((place, i)=>{
+                        const idx = `${i + 1}.`.green;
+                        console.log(`${idx} ${place}`);
+                    })
+
+            break;
         }
 
         if (opt) await pause();
